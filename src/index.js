b7376e55584a63002cb1dde8dd875e5361361231
SivaTejaArla <sivatejaarla.com>
2024-11-15 10:48:56 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const startServer = require('./../server')
-startServer(8500);
+const express = require('express');
+const app  = express();
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Express App ');
+});
+app.get('/about',(req,res)=>{
+    res.status(200).send('Express About Page ');
+});
+app.post('/',(req,res)=>{
+    res.status(200).send('Express Post App ');
+});
+
+app.listen(8500, ()=> {
+    console.log(`Server started at 'http://localhost:8500`);
+})
