85a6df82907f7d464d274a1b65a24e7b487ccc37
SivaTejaArla <sivatejaarla.com>
2024-11-15 10:32:23 +0530

    .

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
-const http = require ('http');
+
 const fs = require('fs')
 const url = require('url');
 
-
 // Read the file
-const da = fs.readFileSync('./src/sample.json', 'utf-8' , (err,data)=>{});
-const dao = JSON.parse(da);
-const server  =  http.createServer( ( req,res ) => {
-
- const path = req.url;
- if ( path == '/overview')
-    res.end("OverView");
-    else if ( path == '/about' )
-    res.end("About");
-    else if ( path == '/contact' )
-    res.end("Contact");
-    else if (path == '/api'){
-       res.end(dao);
-       
-    }
-    else  {
-    res.writeHead(404);    // go to ‘network’ in inspect to see this.
-    res.end("404 ERROr")  }          
+function startServer(port) {
+    const http = require ('http');
+    const server  =  http.createServer( ( req,res ) =>{
+        res.end("Server Started ");
+    });
+    
+    server .listen(port,()=> {
+        console.log(`Server started at 'http://localhost:${port}'`);
+    });
     
 
-
-});
-
-server .listen(7000,()=> {
-    console.log("Server started at port : 7000  ")
-});
+}
+module.exports = startServer;
